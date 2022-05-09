3a3e769e792c24e68f410e51f450c124c8e41a4b
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-09 16:46:44 -0400

    port variable correction

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-//const api = require('./routes')
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-//app.use('/api', api);
 
 app.use(express.static('public'));
 
